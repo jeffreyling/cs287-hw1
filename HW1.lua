fc2d5109325a36a1243c26bbf759e0035e957556
Rohil Prasad <r0hil.pras4d@gmail.com>
2016-02-05 14:49:00 -0500

    Add logistic regression training function

--- HW1.lua
+++ HW1.lua
@@ -9,6 +9,8 @@ cmd:option('-classifier', 'nb', 'classifier to use')
 
 -- Hyperparameters
 cmd:option('-alpha', 19, 'alpha for naive Bayes')
+cmd:option('-eta', 0.01, 'learning rate for SGD')
+cmd:option('-batchsize', 10, 'batch size for SGD')
 
 function train_nb(nclasses, nfeatures, X, Y, alpha)
   -- Trains naive Bayes model
@@ -40,6 +42,65 @@ function NLL(pred, Y)
   -- Returns negative log-likelihood error.
   local probs = pred:index(2, Y:long())
   return -probs:log():sum()
+end
+
+function sgd_step(X_batch, Y_batch, W, b, eta)
+    local N = X_batch:size(1)
+    local indices = torch.linspace(1, N, N):long()
+    local z = torch.Tensor(N, nclasses):fill(0)
+    local Y_hat = torch.Tensor(N, nclasses):fill(0)
+    local z_grad = torch.Tensor(N, nclasses):fill(0)
+
+    for i = 1, N do
+      -- get predictions
+      z[i] = W:index(2, X_batch[i]:long()):sum(2)
+      -- get softmax
+      local max = z[i]:max()
+      Y_hat[i] = z[i]:csub(max):exp()
+      Y_hat[i]:div(Y_hat[i]:sum())
+      -- get gradient w.r.t. z
+      z_grad[i] = Y_hat[i]:mul(-1)
+      z_grad[i][Y_batch[i]] = z_grad[i][Y_batch[i]] + 1
+    end
+
+    -- collapse and increment weights
+    z_grad = z_grad:mean(1):transpose(1,2):mul(eta)
+    b:add(z_grad)
+    W:add(z_grad:expand(W:size(1),W:size(2)))
+    return W, b
+end
+
+function train_logreg(nclasses, nfeatures, X, Y, eta, batchsize)
+  eta = eta or 0
+  batchsize = batchsize or 0
+  local N = X:size(1)
+
+  -- initialize weights and intercept
+  local W = torch.Tensor(nclasses, nfeatures):fill(0)
+  local b = torch.Tensor(nclasses, 1):fill(0)
+
+  local loss = 100
+  while loss > 10 do
+    -- get batch
+    local batch_indices = torch.multinomial(torch.ones(1, N), batchsize):long()
+    X_batch = X:index(1, batch_indices[1])
+    Y_batch = Y:index(1, batch_indices[1])
+
+    -- update weights
+    W, b = sgd_step(X_batch, Y_batch, W, b, eta)
+    
+    -- calculate loss
+    local pred = torch.zeros(N, nclasses)
+    local cross_entropy = torch.zeros(N, 1)
+    for i = 1, N do
+      pred[i] = W:index(2, X[i]:long()):sum(2)
+      local max = pred[i]:max()
+      cross_entropy[i] = pred[i]:csub(max - math.log(pred[i]:csub(max):exp():sum()))[Y[i]]
+    end
+    loss = cross_entropy:sum()
+    print(loss)
+  end
+  return W, b
 end
 
 function eval(X, Y, W, b, nclasses, model)
@@ -81,7 +142,9 @@ function main()
    --local b = torch.zeros(nclasses)
 
    -- Train.
-   local W, b = train_nb(nclasses, nfeatures, X, Y, opt.alpha)
+   -- local W, b = train_nb(nclasses, nfeatures, X, Y, opt.alpha)
+   local W, b = train_logreg(nclasses, nfeatures, X, Y, opt.eta, opt.batchsize)
+   os.exit()
 
    -- Test.
    local pred, err = eval(valid_X, valid_Y, W, b, nclasses)
