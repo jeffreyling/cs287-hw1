52af16451b4c021d827ef8828ef315c53ed067c8
Jeffrey Ling <jling@college.harvard.edu>
2016-02-06 15:10:42 -0500

    logreg def working now

--- HW1.lua
+++ HW1.lua
@@ -8,8 +8,8 @@ cmd:option('-datafile', 'SST1.hdf5', 'data file')
 cmd:option('-classifier', 'nb', 'classifier to use')
 
 -- Hyperparameters
-cmd:option('-alpha', 0.1, 'alpha for naive Bayes')
-cmd:option('-eta', 0.01, 'learning rate for SGD')
+cmd:option('-alpha', 1, 'alpha for naive Bayes')
+cmd:option('-eta', 0.001, 'learning rate for SGD')
 cmd:option('-batch_size', 50, 'batch size for SGD')
 cmd:option('-max_epochs', 1000, 'max # of steps for SGD')
 
@@ -32,6 +32,7 @@ function train_nb(nclasses, nfeatures, X, Y, alpha)
   -- zero out padding counts
   W:select(2, 1):zero()
   local W_logsum = W:sum(2):log()
+  print(W_logsum)
   W:log():csub(W_logsum:expand(W:size(1), W:size(2)))
   -- padding weight to zero
   W:select(2, 1):zero()
@@ -52,10 +53,14 @@ end
 function linear(X, W, b)
   -- performs y = softmax(x*W + b)
   local N = X:size(1)
-  local z = torch.zeros(N, nclasses)
+  local k = X:size(2)
+  local z = torch.zeros(N, W:size(1))
   for i = 1, N do
     -- get predictions
-    z[i] = W:index(2, X[i]:long()):sum(2)
+    local feats = torch.zeros(W:size(2), 1)
+    feats:indexFill(1, X[i]:long(), 1)
+    feats:squeeze()
+    z[i] = W[{{}, feats:long()}]:sum(2)
     z[i]:add(b)
   end
   -- get softmax
@@ -114,10 +119,10 @@ function train_logreg(nclasses, nfeatures, X, Y, eta, batch_size, max_epochs)
     local W_grad, b_grad = sgd_grad(X_batch, Y_batch, W, b)
 
     -- numerical grads
-    --local eps = 0.01
-    --local y1 = linear(X_batch, W, b + torch.Tensor{eps,0,0,0,0})
-    --local y2 = linear(X_batch, W, b - torch.Tensor{eps,0,0,0,0})
-    --print((NLL(y1, Y_batch) - NLL(y2, Y_batch)) / (2 * eps))
+    --local eps = 1e-5
+    --local y1 = linear(X_batch, W, b + torch.Tensor{0,eps,0,0,0})
+    --local y2 = linear(X_batch, W, b - torch.Tensor{0,eps,0,0,0})
+    --print((NLL(y1, Y_batch) - NLL(y2, Y_batch)) / (2 * eps * batch_size))
     --print(b_grad)
     --io.read()
 
@@ -149,7 +154,6 @@ end
 
 function eval(X, Y, W, b, nclasses)
   -- Returns error from Y
-
   local _, pred = linear(X, W, b)
   
   -- Compute error from Y
@@ -157,6 +161,15 @@ function eval(X, Y, W, b, nclasses)
   argmax:squeeze()
   local err = argmax:eq(Y:long()):sum() / Y:size(1)
   return argmax, err
+end
+
+function test()
+  local X = torch.range(1, 12):reshape(3, 4)
+  local W = torch.range(1, 24):reshape(2, 12)
+  local b = torch.Tensor{1, 2}
+  print(X, W, b)
+  local y, z = linear(X, W, b)
+  print(y, z)
 end
 
 function main() 
@@ -180,9 +193,9 @@ function main()
      W, b = train_nb(nclasses, nfeatures, X, Y, opt.alpha)
    elseif opt.classifier == 'logreg' then
      -- sample for faster training
-     local batch_indices = torch.multinomial(torch.ones(X:size(1)), 100, false):long()
-     X = X:index(1, batch_indices)
-     Y = Y:index(1, batch_indices)
+     --local batch_indices = torch.multinomial(torch.ones(X:size(1)), 100, false):long()
+     --X = X:index(1, batch_indices)
+     --Y = Y:index(1, batch_indices)
      W, b = train_logreg(nclasses, nfeatures, X, Y, opt.eta, opt.batch_size, opt.max_epochs)
    end
 
